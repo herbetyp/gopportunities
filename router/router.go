e3f256c4bb88ea5e0afe60dc1e7c9dec06721351
herbetyp <herbetypaulo@gmail.com>
2024-07-06 00:03:21 -0300

    feat: add routes logic

--- router/router.go
+++ router/router.go
@@ -1,13 +1,13 @@
 package router
 
-import "github.com/gin-gonic/gin"
+import (
+	"github.com/gin-gonic/gin"
+)
 
 func Init() {
-	r := gin.Default()
-	r.GET("/ping", func(c *gin.Context) {
-		c.JSON(200, gin.H{
-			"message": "pong",
-		})
-	})
-	r.Run(":8080") // listen and serve on 0.0.0.0:8080
+	router := gin.Default()
+
+	initRoutes(router)
+
+	router.Run(":8080")
 }
